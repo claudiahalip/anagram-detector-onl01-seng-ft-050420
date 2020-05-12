89844b453c7eac773da29c64f6352fbb349a9d05
claudia v <claudia4ro@yahoo.com>
2020-05-12 14:11:06 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+
+class Anagram 
+  attr_accessor :word
+  
+  def initialize (word)
+    @word = word
+  end
+    
+  def match(array_posible_words)
+    word = self.word
+    array_posible_words.select { |anagram|
+       word.split.sort.join == anagram.split.sort.join}
+       
+        
+  end
+  
+end
